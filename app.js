f61d430be2155459864971d7bbfe884330d3b477
muchuan <mu.chuan.hung@gmail.com>
2021-10-07 17:52:06 +0800

    Basic Setting

---
+++ app.js
@@ -0,0 +1,29 @@
+// app.js
+// require packages used in the project
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+// setting template engine
+app.engine('handlebars', exphbs ({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// setting static files 設定Express路由以提供靜態檔案
+app.use(express.static('public'))
+
+// setting routes
+app.get('/', (req, res) => {
+// past the data into 'index' partial template
+  res.render('index')
+})
+
+app.get('/:page', (req, res) => {
+  const pageName = req.params.page
+  res.render('index', { pageName })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express app is running on the http://localhost:${port}`)
+})
